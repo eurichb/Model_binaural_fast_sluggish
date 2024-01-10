5537fcc8bcf92279c99145496f598d1ce3e0e1df
eurichb <74491861+eurichb@users.noreply.github.com>
2024-01-10 12:06:23 +0100

    Update simulate_KollmeierGilkey1990.m

--- simulate_KollmeierGilkey1990.m
+++ simulate_KollmeierGilkey1990.m
@@ -34,7 +34,7 @@ third_split = {'noise_mode','spar'};
 %% Definitions
 % pc_threshold = 0.707; % Proportion of correct responses to be defined as detection threshold
 dprime_threshold =1.14; %0.78; % d' at threshold
-mpar = Eurich2022mpar;
+mpar = Eurich2023mpar;
 
 %% Experiment-specific internal noise parameter set
 
